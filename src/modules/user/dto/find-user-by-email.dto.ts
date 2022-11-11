d85b6c3f0e80c62208e91352e046315b2c69146a
Triska Mahfud K <triska.triskacode@gmail.com>
2022-11-11 10:08:39 +0000

    update dto

--- src/modules/user/dto/find-user-by-email.dto.ts
+++ src/modules/user/dto/find-user-by-email.dto.ts
@@ -1,8 +1,3 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
-
 export class FindUserByEmailDto {
-  @IsEmail()
-  @IsString()
-  @IsNotEmpty()
   email: string;
 }
--- src/modules/user/dto/find-user-by-id.dto.ts
+++ src/modules/user/dto/find-user-by-id.dto.ts
@@ -1,7 +1,3 @@
-import { IsNotEmpty, IsNumberString } from 'class-validator';
-
 export class FindUserByIdDto {
-  @IsNumberString()
-  @IsNotEmpty()
   id: number;
 }
